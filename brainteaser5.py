cf1e193ae79a806f3091672395c048b286dd3e07
Areebah-snow <85735515+Areebah-snow@users.noreply.github.com>
2023-01-19 13:10:38 +0100

    Update brainteaser5.py

--- brainteaser5.py
+++ brainteaser5.py
@@ -1,33 +1,3 @@
-x = 500
-print(type(x))
-<class 'int'>
-y = "apple"
-print(type(y))
-<class 'str'>
-z = 3.142
-print(type(z))
-<class 'float'>
-isTrue = True
-print(type(isTrue))
-<class 'bool'>
-x = 100
-print(isinstance(x,int))
-True
-print(isinstance(y,float))
-False
-y = 100.9
-print(isinstance(y,float))
-True
-z = 1.002
-print(isinstance(z,float))
-True
-z = 1.003
-print(isinstance(z,float))
-True
-print(isinstance(y,int))
-False
-
-
 studentID = 12344
 print(isinstance(studentID,int))
 True
