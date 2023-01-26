5a9a806c8af5830b1c4bbcc2e1f38ec00cd938c4
Areebah-snow <85735515+Areebah-snow@users.noreply.github.com>
2023-01-26 13:00:30 +0100

    Uploading brainteaser

---
+++ brainteaser5.py
@@ -0,0 +1,12 @@
+book1 = "imagination"
+book2 = "conceptualization"
+book3 = "intelligence"
+book4 = "consciousness"
+
+print(book3[2:6])
+print(book2[5:0])
+print(book3[4:7])
+print(len(book1))
+print(len(book2))
+print(len(book3))
+print(len(book4))
