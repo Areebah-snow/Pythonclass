90d952304f7fae6fd09c62349002107bdbb18c5d
Areebah-snow <85735515+Areebah-snow@users.noreply.github.com>
2023-02-02 11:14:04 +0100

    Add files via upload

---
+++ brainteaser7.py
@@ -0,0 +1,17 @@
+print('''
+    ###   ####  $$$$$$$  #######  %%%%%%%  #$#$#$#  #$#%#%$   #$%$%#$    %#$#%#
+    ###  ###      $$$      ###      %%%      #$#      #$%     #$        #
+    #####         $$$      ###      %%%      #$#      #$%     #%$#%#$   $#@#$#%
+    ###  ###      $$$      ###      %%%      #$#      %$#     #%              $
+    ###   ####  $$$$$$$    ###      %%%      $#$    #$%#$%#   #$%%$%$   #$$%%#
+
+    ''');
+
+
+Kitties = ["fried rice and chicken","abula and gbegiri with egunfe","yam pottage","ikokore","chicken and chips"]
+                  
+customer_order = ["fried rice and chicken",,"chicken and chips""]
+                  
+SyntaxError: unterminated string literal (detected at line 1)
+print(len("SyntaxError"))
+                  
